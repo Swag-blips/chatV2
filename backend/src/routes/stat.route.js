d691e264bc4bf70e79708f4c2f929c8af3c878d0
Swag-blips <coderblip@gmail.com>
2024-11-09 15:11:04 +0100

    feat:song controller complete

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,6 +1,8 @@
 import Router from "express";
+import { protectRoute, requiredAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controllers/stat.controller.js";
 
 const router = Router();
 
-router.get("/");
+router.get("/", protectRoute, requiredAdmin, getStats);
 export default router;
