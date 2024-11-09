aa8a72f00013e54c6d289e415ee9a1b67a640a48
Swag-blips <coderblip@gmail.com>
2024-11-09 12:25:42 +0100

    feat:check-admin

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,17 @@
 import Router from "express";
 import { protectRoute, requiredAdmin } from "../middleware/auth.middleware.js";
-import { createSong, deleteSong } from "../controllers/admin.controller.js";
+import {
+  createAlbum,
+  createSong,
+  deleteAlbum,
+  deleteSong,
+} from "../controllers/admin.controller.js";
 
 const router = Router();
-
+router.length("/check", protectRoute, requiredAdmin, checkAdmin);
 router.post("/songs", protectRoute, requiredAdmin, createSong);
 router.delete("/songs/:id", protectRoute, requiredAdmin, deleteSong);
+router.post("/albums", protectRoute, requiredAdmin, createAlbum);
+router.delete("/albums/:id", protectRoute, requiredAdmin, deleteAlbum);
+
 export default router;
