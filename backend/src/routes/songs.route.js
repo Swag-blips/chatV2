e471743aa274ab7ea2b48872d5bde4705e825ed1
Swag-blips <coderblip@gmail.com>
2024-11-09 14:29:34 +0100

    songs endpoint, complete

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,8 +1,16 @@
 import Router from "express";
+import { protectRoute, requiredAdmin } from "../middleware/auth.middleware.js";
+import {
+  getAllSongs,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/songs.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("song route with get method");
-});
+router.get("/", protectRoute, requiredAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 export default router;
