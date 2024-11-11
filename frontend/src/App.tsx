dac3d702c6f49a6d1c4e967842cd59e3622c6a6e
Swag-blips <coderblip@gmail.com>
2024-11-11 11:22:29 +0100

    feat:routes setup axios init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth/AuthCallbackPage";
+import { axiosInstance } from "./lib/axios";
 
 export default function App() {
+  
+
   return (
     <>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/auth/AuthCallbackPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthCallbackPage = () => {
+  return <div>authCallbackPage</div>;
+};
+
+export default AuthCallbackPage;
