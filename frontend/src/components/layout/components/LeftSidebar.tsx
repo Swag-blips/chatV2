57531cfdecf4703b6c8b4395ec9b048f497be026
Swag-blips <coderblip@gmail.com>
2024-11-12 05:26:52 +0100

    feat:left-sidebar skeleton

--- frontend/src/components/layout/components/LeftSidebar.tsx
+++ frontend/src/components/layout/components/LeftSidebar.tsx
@@ -1,9 +1,13 @@
+import PlaylistSkeleton from "@/components/skeletons/PlayListSkeleton";
 import { buttonVariants } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import { HomeIcon } from "lucide-react";
+import { SignedIn } from "@clerk/clerk-react";
+import { HomeIcon, Library, MessageCircle } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
+  const isLoading = true;
   return (
     <div className="h-full flex flex-col gap-2">
       <div className="rounded-lg bg-zinc-900 p-4">
@@ -20,7 +24,37 @@ const LeftSidebar = () => {
             <HomeIcon className="mr-2 size-5" />
             <span className="hidden md:inline">Home</span>
           </Link>
+
+          <SignedIn>
+            <Link
+              to={"/chat"}
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                  className:
+                    "w-full justify-start text-white hover:bg-zinc-800",
+                })
+              )}
+            >
+              <MessageCircle className="mr-2 size-5" />
+              <span className="hidden md:inline">Chat</span>
+            </Link>
+          </SignedIn>
         </div>
+      </div>
+      <div className="flex-1 rounded-lg bg-zinc-900 p-4 ">
+        <div className="flex items-center  mb-4 ">
+          <div className="flex items-center text-white px-4">
+            <Library className="size-5 mr-2" />
+            <span className="hidden md:inline">Playlists</span>
+          </div>
+        </div>
+
+        <ScrollArea className="h-[calc(100vh-300px)] ">
+          <div className="space-y-2">
+            {isLoading ? <PlaylistSkeleton /> : "some music"}
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
