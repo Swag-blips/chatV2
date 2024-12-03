7953fa6feb23fa07e106455d1f959c6525fd3165
Swag-blips <coderblip@gmail.com>
2024-12-03 17:43:09 +0100

    total refacotr

--- backend/src/routes/stat.route.js
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import { protectRoute, requiredAdmin } from "../middleware/auth.middleware.js";
-import { getStats } from "../controllers/stat.controller.js";
-
-const router = Router();
-
-router.get("/", protectRoute, requiredAdmin, getStats);
-export default router;
