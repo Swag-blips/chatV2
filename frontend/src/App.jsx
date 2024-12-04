9c0ffab34096744433d4faeceb76b2ad2241e818
Swag-blips <coderblip@gmail.com>
2024-12-04 10:24:50 +0100

    setup:routes and their individual pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>avbar</div>;
+};
+
+export default Navbar;
