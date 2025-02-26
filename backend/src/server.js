8498380f71e6b890bd182672b1f072f6172f056a
Swag-blips <coderblip@gmail.com>
2025-02-26 07:48:50 +0100

    feat:socket server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,10 @@ import dotenv from "dotenv";
 import connectDb from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+
 app.use(express.json({ limit: "5mb" }));
 app.use(
   cors({
@@ -21,7 +22,7 @@ app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDb();
   console.log(`Server is running on ${PORT}`);
 });
